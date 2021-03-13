466328b636f76f9e30d9417ca7d58a33e3110389
Christopher DeRoche <46451756+compact-disc@users.noreply.github.com>
2021-03-13 14:45:11 -0500

    Added log message when bot leaves voice channel when empty

--- src/main/java/com/cdero/gamerbot/events/GuildVoiceLeaveEventListener.java
+++ src/main/java/com/cdero/gamerbot/events/GuildVoiceLeaveEventListener.java
@@ -2,6 +2,8 @@ package com.cdero.gamerbot.events;
 
 //import statements
 import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import com.cdero.gamerbot.audio.lavaplayer.MusicManagers;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceLeaveEvent;
@@ -15,6 +17,11 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
  *
  */
 public class GuildVoiceLeaveEventListener extends ListenerAdapter {
+	
+	/**
+	 * Logger for the GuildVoiceLeaveEventListener class.
+	 */
+	private final static Logger log = LogManager.getLogger(GuildVoiceLeaveEventListener.class.getName());
 	
 	/**
 	 * Method that is triggered when a user leaves 
@@ -32,6 +39,9 @@ public class GuildVoiceLeaveEventListener extends ListenerAdapter {
 				
 				event.getGuild().getAudioManager().closeAudioConnection();
 				MusicManagers.musicManagers.get(Long.parseLong(event.getGuild().getId())).player.destroy();
+				
+				log.info("Event: Guild Voice Leave"
+						+ "\nGuild: " + event.getGuild().getName());
 				
 			}
 
